4cda66b55cedf16eda90cf0bb580a30fc6cb8ccb
shanthikp246 <shanthikp@gmail.com>
2024-05-27 12:35:29 -0700

    Add imagePullPolicy config

---
+++ deployment.sh
@@ -0,0 +1,11 @@
+kubectl apply -f k8s/deployment-rabbitmq.yaml
+
+kubectl apply -f k8s/deployment-fastapi.yaml
+kubectl apply -f k8s/service-fastapi.yaml
+
+kubectl apply -f k8s/deployment-celery.yaml
+kubectl apply -f k8s/service-celery.yaml
+
+kubectl apply -f k8s/hpa-celery.yaml
+kubectl apply -f k8s/hpa-fastapi.yaml
+

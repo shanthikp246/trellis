bae0e619a208e251650007cd8dc030da88a75e2d
shanthikp246 <shanthikp@gmail.com>
2024-07-11 12:58:43 -0700

    Fix readme and add loadbalancer type

--- test/load_test.py
+++ test/load_test.py
@@ -3,14 +3,14 @@ import json
 import numpy as np
 
 # Generate large matrices
-matrix_size = 1000
+matrix_size = 900
 matrix1 = np.random.rand(matrix_size, matrix_size).tolist()
 matrix2 = np.random.rand(matrix_size, matrix_size).tolist()
 
 # Endpoint URL
 url = "http://localhost:8080/tasks"
 
-print(matrix1)
+print(f"{matrix_size=}")
 
 # Define the payload
 payload = {
@@ -19,8 +19,9 @@ payload = {
 }
 
 # Send burst of requests
-num_requests = 1
-for _ in range(num_requests):
+num_requests = 10
+for i in range(num_requests):
+    print(f"request: {i}")
     response = requests.post(url, json=payload)
     print(response.status_code)
     if response.status_code == 200:
